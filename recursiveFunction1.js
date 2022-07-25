19de574619f49182f3bfa35b0b2dcb0c0cbf22b6
Maryna Markova <markova.maryna@gmail.com>
2022-07-25 15:48:57 +0300

    add a non-recursive solution to RecursiveFunction.js

--- recursiveFunction1.js
+++ recursiveFunction1.js
@@ -8,4 +8,13 @@ function countDown(num) {
   countDown(num);
 }
 
+// without recursion
+
+// function countDown(num) {
+//   for (var i = num; i > 0; i--) {
+//     console.log(i);
+//   }
+//   console.log("All done!");
+// }
+
 countDown(5);
