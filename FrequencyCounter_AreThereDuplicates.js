082ff3815ee653002fed3dcc8f6de8775e51d26f
Maryna Markova <markova.maryna@gmail.com>
2022-07-27 11:41:05 +0300

    add Frequecy Counter - Are There Duplicates algorithm

---
+++ FrequencyCounter_AreThereDuplicates.js
@@ -0,0 +1,14 @@
+//areThereDuplicates(1, 2, 3) //false
+areThereDuplicates(1, 2, 2); //true
+//areThereDuplicates('a', 'b', 'c', 'a') //true
+
+function areThereDuplicates() {
+  let collection = {};
+  for (let val in arguments) {
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) return true;
+  }
+  return false;
+}
