cd3b3e2abeea061cc3f84bf7c3f14200194d2bf3
Maryna Markova <markova.maryna@gmail.com>
2022-08-12 22:24:01 +0300

    create a String Search Naive algorithm file

---
+++ Algorithms/Search_naiveString.js
@@ -0,0 +1,14 @@
+//Count the number of times a smaller string appears in a longer string
+
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+naiveSearch("lorie loled", "lo");
