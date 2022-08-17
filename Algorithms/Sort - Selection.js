54bee2f37e654dfd0d3dfc5506d393e9b3e36293
Maryna Markova <markova.maryna@gmail.com>
2022-08-17 12:23:57 +0300

    create a Selection Sort Algorithm

---
+++ Algorithms/Sort - Selection.js
@@ -0,0 +1,33 @@
+// ES5
+// function selectionSort(arr){
+//      for (let i=0; i<arr.length; i++){
+//             let lowest = i;
+//             for (let j = i+1; j<arr.length; j++) {
+//             if (arr[j]< arr[lowest])lowest = j;
+//             }
+//          if (i !== lowest) {
+//             let temp = arr[i];
+//             arr[i] = arr[lowest];
+//             arr[lowest] = temp;
+//         }
+//     }
+//     return arr;
+// }
+
+// ES2015
+
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) lowest = j;
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+  return arr;
+}
+
+selectionSort([2, 4, -1, 15, 0, 3]);
